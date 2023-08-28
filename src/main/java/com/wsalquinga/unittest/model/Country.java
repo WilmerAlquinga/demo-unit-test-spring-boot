18e0f2b30a4fd09924d8d6e273a04ebdd5bbda12
wsalquinga <wilmer_alquinga@outlook.es>
2023-08-28 01:16:54 -0500

    change db h2 to mysql8

--- src/main/java/com/wsalquinga/unittest/model/Country.java
+++ src/main/java/com/wsalquinga/unittest/model/Country.java
@@ -1,15 +1,16 @@
 package com.wsalquinga.unittest.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Builder;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Country {
 
 	@Id
@@ -29,4 +30,19 @@ public class Country {
 	@Column(name = "country_independence_date")
 	private String countryIndependenceDate;
 
+	@Override
+	public final boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null) return false;
+		Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+		Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+		if (thisEffectiveClass != oEffectiveClass) return false;
+		Country country = (Country) o;
+		return getCountryId() != null && Objects.equals(getCountryId(), country.getCountryId());
+	}
+
+	@Override
+	public final int hashCode() {
+		return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+	}
 }
